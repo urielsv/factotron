857b861d565fe6a03bb6b1276d598e5f94a15d00
Uriel Sosa Vazquez <urielsv@uriel-m2.fibertel.com.ar>
2023-02-13 00:09:21 -0300

    Factotron discord bot.

---
+++ src/app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const { Client, IntentsBitField, messageLink } = require('discord.js');
+const fs = require('fs');
+const { listenerCount } = require('process');
+const { callbackify } = require('util');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} running.`)
+});
+
+client.on('interactionCreate', (i) => {
+    if (!i.isChatInputCommand()) return;
+
+    if (i.commandName === 'addfacto') {
+        const fact = i.options.get('facto').value;
+        console.log(fact);
+        fs.appendFile('fact-list.txt', `${fact}\n`, err => {});
+
+        i.reply(`📝 ** Facto agregado! ** (${fact})`);
+    }
+});
+
+client.on('messageCreate', (msg) => {
+    if (msg.content === "facto") {
+        const data = fs.readFileSync('fact-list.txt', 'utf8');
+        var lines = data.split('\n');
+
+        var line = lines[Math.floor(Math.random() * lines.length)]
+        console.log(line);
+        msg.reply(`${line}`);
+    }
+});
+
+client.login(process.env.TOKEN)
---
+++ src/register-commands.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
+
+const commands = [
+    {
+        name: 'addfacto',
+        description: 'Agregar un facto.',
+        options: [
+            {
+                name: 'facto',
+                description: 'Mensaje del factazo.',
+                type: ApplicationCommandOptionType.String,
+                required: true,
+            },
+        ],
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('Registering slash commands...');
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        )        
+        console.log('Slash commands were registered successfully!');
+    } catch (e) {
+        console.log(`Error: ${e}`);
+    }
+})();
